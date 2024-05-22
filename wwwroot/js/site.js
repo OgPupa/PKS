eacc7f68064cdbf7d9dcb0a4d89754227a1112cb
OgPupa <125026631+OgPupa@users.noreply.github.com>
2024-05-22 16:42:57 +0300

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -13,9 +13,6 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
-                <!-- Registration form goes here -->
-            </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
                 <button type="submit" class="btn btn-primary">Сохранить</button>
